b1ae13b2d0793c3c2d7eb74caacdffa1275cdeb8
Ashish <Ashish@192.168.0.107>
2023-06-28 21:26:33 +0530

    Initial Commit.

---
+++ StudentManagementSystem/src/com/student/Service/NotFoundException.java
@@ -0,0 +1,13 @@
+package com.student.Service;
+
+public class NotFoundException extends RuntimeException{
+public NotFoundException()
+{
+System.out.println("Student not found in the system");
+}
+public NotFoundException(String msg)
+{
+super(msg);	
+}
+
+}
