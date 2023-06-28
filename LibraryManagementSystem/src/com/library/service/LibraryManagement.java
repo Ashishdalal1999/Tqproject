4492995d9b8398b93093f2027e10a56f31087a26
Ashish <Ashish@192.168.0.107>
2023-06-28 21:32:52 +0530

    Library

---
+++ LibraryManagementSystem/src/com/library/service/LibraryManagement.java
@@ -0,0 +1,10 @@
+package com.library.service;
+
+public interface LibraryManagement {
+void addBook( );
+void removeBook( );
+void searchByTitle( );
+void searchByAuthor( );
+void displayAllBooks();
+void displayBooksByGenre();
+}
